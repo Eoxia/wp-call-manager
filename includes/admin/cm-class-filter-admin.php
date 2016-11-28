01da4a74410184bc6030fb1872c81c48afe71eac
Zerdiron <erdiron@hotmail.fr>
2016-11-28 17:12:42 +0100

    update

--- includes/admin/cm-class-filter-admin.php
+++ includes/admin/cm-class-filter-admin.php
@@ -37,7 +37,7 @@ class Cm_Filter_Admin {
 		$number_call = 0;
 		$number_blame = 0;
 		$selection = get_user_meta( $user_id, 'imputation_' . $year . '' . $month, true );
-		if ( ! empty( $selection ) ) {
+		if ( ! empty( $selection ) ) { // TOTAL DES BLAME & CALL.
 			if ( null === $day ) {
 				foreach ( $selection as $key => $call ) {
 					$number_call = $number_call + $selection[ $key ]['call'];
@@ -52,7 +52,59 @@ class Cm_Filter_Admin {
 				}
 			}
 		}
+
 		include( plugin_dir_path( __FILE__ ) . 'views/task-manager/summary-filter.php' );
+
+		$select = get_user_meta( get_current_user_id(),'imputation_' . $year . $month, true );
+		$id_select = get_users( 'orderby=nicename&role=administrator&exclude=' . get_current_user_id() . '' );
+		$i = 0;
+		if ( ! empty( $id_select ) ) { // DIV DES RECAP.
+			if ( null === $day ) { // MOIS EN COURS.
+				for ( $m = 0; $m <= 31; $m++ ) {
+					$cm_month[] = $m;
+				}
+				$nbr_total_blame_inconnu = 0;
+				$nbr_total_blame_stagiaire = 0;
+				$cm_recap_month_array = array();
+				foreach ( $cm_month as $non_day ) {
+					if ( isset( $select[ $non_day ]['blame']['0'] ) and ( 0 !== $select[ $non_day ]['blame']['0'] ) ) {
+						$nbr_total_blame_inconnu = $nbr_total_blame_inconnu + $select[ $non_day ]['blame']['0'];
+					}
+					if ( isset( $select[ $non_day ]['blame']['999999'] ) and ( 0 !== $select[ $non_day ]['blame']['999999'] ) ) {
+						$nbr_total_blame_stagiaire = $nbr_total_blame_stagiaire + $select[ $non_day ]['blame']['999999'];
+					}
+					foreach ( $id_select as $user ) {
+						if ( ! empty( $select[ $non_day ]['blame'][ $user->ID ] ) ) {
+							$name = ucfirst( $user->display_name );
+							$nbr_blame = $select[ $non_day ]['blame'][ $user->ID ];
+							if ( ! isset( $cm_recap_month_array[ $name ] ) ) {
+								$cm_recap_month_array[ $name ] = 0;
+							}
+							$cm_recap_month_array[ $name ] = $cm_recap_month_array[ $name ] + $nbr_blame;
+						}
+					}
+				}
+				$cm_recap_month_array['Inconnus'] = $nbr_total_blame_inconnu;
+				$cm_recap_month_array['Stagiaires'] = $nbr_total_blame_stagiaire;
+			}
+			if ( null !== $day ) { // JOUR COURRANT.
+				$cm_recap_day_array = array();
+				if ( isset( $select[ $day ]['blame']['0'] ) and ( 0 !== $select[ $day ]['blame']['0'] ) ) {
+					$cm_recap_day_array['Inconnus'] = $select[ $day ]['blame']['0'];
+				}
+				if ( isset( $select[ $day ]['blame']['999999'] ) and ( 0 !== $select[ $day ]['blame']['999999'] ) ) {
+					$cm_recap_day_array['Stagiaires'] = $select[ $day ]['blame']['999999'];
+				}
+				foreach ( $id_select as $user ) {
+					if ( ! empty( $select[ $day ]['blame'][ $user->ID ] ) ) {
+						$name = ucfirst( $user->display_name );
+						$nbr_blame = $select[ $day ]['blame'][ $user->ID ];
+						$cm_recap_day_array[ $name ] = $nbr_blame;
+					}
+				}
+			}
+		}
+
 		include( plugin_dir_path( __FILE__ ) . 'views/task-manager/summary-call-recap.php' );
 		include( plugin_dir_path( __FILE__ ) . 'views/task-manager/summary-blame-recap.php' );
 	}
--- includes/admin/views/task-manager/summary-blame-recap.php
+++ includes/admin/views/task-manager/summary-blame-recap.php
@@ -17,85 +17,33 @@ if ( null !== $day ) {
 }
 ?>
 >
-	<?php
-	$select = get_user_meta( get_current_user_id(),'imputation_' . $year . $month, true );
-	$total_blame = 0;
-	$id_select = get_users( 'orderby=nicename&role=administrator&exclude=' . get_current_user_id() . '' );
-	if ( ! empty( $id_select ) ) {
-		$i = 0;
-		if ( null === $day ) {
-			for ( $m = 0; $m <= 31; $m++ ) {
-				$cm_month[] = $m;
-			}
-			$nbr_total_blame_inconnu = 0;
-			$nbr_total_blame_stagiaire = 0;
-			$cm_recap_month_array = array();
-			foreach ( $cm_month as $non_day ) {
-				if ( isset( $select[ $non_day ]['blame']['0'] ) and ( 0 !== $select[ $non_day ]['blame']['0'] ) ) {
-					$nbr_total_blame_inconnu = $nbr_total_blame_inconnu + $select[ $non_day ]['blame']['0'];
-				}
-				if ( isset( $select[ $non_day ]['blame']['999999'] ) and ( 0 !== $select[ $non_day ]['blame']['999999'] ) ) {
-					$nbr_total_blame_stagiaire = $nbr_total_blame_stagiaire + $select[ $non_day ]['blame']['999999'];
-				}
-				foreach ( $id_select as $user ) {
-					if ( ! empty( $select[ $non_day ]['blame'][ $user->ID ] ) ) {
-						$name = ucfirst( $user->display_name );
-						$nbr_blame = $select[ $non_day ]['blame'][ $user->ID ];
-						if ( ! isset( $cm_recap_month_array[ $name ] ) ) {
-							$cm_recap_month_array[ $name ] = 0;
-						}
-						$cm_recap_month_array[ $name ] = $cm_recap_month_array[ $name ] + $nbr_blame;
-					}
+	<table>
+			<tr>
+		<?php
+		if ( null !== $day ) {
+			foreach ( $cm_recap_day_array as $name => $nbr_blame_day ) {
+				?>
+				<td> <?php echo esc_html( $name . ' : ' . $nbr_blame_day . '.' ) ?> </td>
+				<td> </td> <td> </td> <td> </td> <td> </td> <td> </td>
+				<?php
+				$i++;
+				if ( 0 === $i % 8 ) {
+					?> </tr> <tr> <?php
 				}
 			}
-			$cm_recap_month_array['Inconnus'] = $nbr_total_blame_inconnu;
-			$cm_recap_month_array['Stagiaires'] = $nbr_total_blame_stagiaire;
+		} elseif ( null === $day ) {
+			foreach ( $cm_recap_month_array as $name => $nbr_blame_month ) {
+				?>
+				<td> <?php echo esc_html( $name . ' : ' . $nbr_blame_month . '.' ) ?> </td>
+				<td> </td> <td> </td> <td> </td> <td> </td> <td> </td>
+				<?php
+				$i++;
+				if ( 0 === $i % 8 ) {
+					?> </tr> <tr> <?php
+				}
+			}
 		}
 		?>
-		<table>
-			<tr>
-			<?php
-			if ( null !== $day ) {
-				$cm_recap_day_array = array();
-				if ( isset( $select[ $day ]['blame']['0'] ) and ( 0 !== $select[ $day ]['blame']['0'] ) ) {
-					$cm_recap_day_array['Inconnus'] = $select[ $day ]['blame']['0'];
-				}
-				if ( isset( $select[ $day ]['blame']['999999'] ) and ( 0 !== $select[ $day ]['blame']['999999'] ) ) {
-					$cm_recap_day_array['Stagiaires'] = $select[ $day ]['blame']['999999'];
-				}
-				foreach ( $id_select as $user ) {
-					if ( ! empty( $select[ $day ]['blame'][ $user->ID ] ) ) {
-						$name = ucfirst( $user->display_name );
-						$nbr_blame = $select[ $day ]['blame'][ $user->ID ];
-						$cm_recap_day_array[ $name ] = $nbr_blame;
-					}
-				}
-				foreach ( $cm_recap_day_array as $name => $nbr_blame_day ) {
-					?>
-					<td> <?php echo esc_html( $name . ' : ' . $nbr_blame_day . '.' ) ?> </td>
-					<td> </td> <td> </td> <td> </td> <td> </td> <td> </td>
-					<?php
-					$i++;
-					if ( 0 === $i % 8 ) {
-						?> </tr> <tr> <?php
-					}
-				}
-			} elseif ( null === $day ) {
-				foreach ( $cm_recap_month_array as $name => $nbr_blame_month ) {
-					?>
-					<td> <?php echo esc_html( $name . ' : ' . $nbr_blame_month . '.' ) ?> </td>
-					<td> </td> <td> </td> <td> </td> <td> </td> <td> </td>
-					<?php
-					$i++;
-					if ( 0 === $i % 8 ) {
-						?> </tr> <tr> <?php
-					}
-				}
-			}
-			?>
-			</tr>
-		</table>
-		<?php
-	}
-	?>
+		</tr>
+	</table>
 </div>
