d30e6b1e00fe2933f47b8e5fb8bacf7a3b054614
Zerdiron <erdiron@hotmail.fr>
2016-12-01 11:33:43 +0100

    test

--- includes/admin/cm-class-send-mail-admin.php
+++ includes/admin/cm-class-send-mail-admin.php
@@ -76,10 +76,13 @@ class Cm_Mail_Sender {
 			$cm_get_email = get_userdata( get_current_user_id() );
 			$to = $cm_get_email->user_email;
 			$sujet = 'Vous devez rappeler des clients !';
-			$message = $contents;
-			wp_mail( $to, $sujet, $message );
+			$message = 'Content-Type: text/html; charset="UTF-8"';
+			$message .= 'Content-Transfer-Encoding: 8bit';
+			$message .= $contents;
+			$header = 'Content-Type: multipart/alternative;';
+			wp_mail( $to, $sujet, $message, $header );
 		}
 	}
 }
 
-// new Cm_Mail_Sender();
+new Cm_Mail_Sender();
