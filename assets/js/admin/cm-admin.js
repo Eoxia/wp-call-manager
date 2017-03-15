979026df5a5432b870011c67a530aff705511189
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-03-15 17:48:19 +0100

    Enlève le bouton "rechercher"

--- assets/js/admin/cm-admin.js
+++ assets/js/admin/cm-admin.js
@@ -58,27 +58,27 @@ jQuery( document ).ready( function( $ ) {
 		width: "auto",
 		modal: true,
 		buttons: {
-			Rechercher: function() {
-				var emailCheck = jQuery( "#form-dialog #email_contact_call" ).val();
-				if ( validateEmail( emailCheck ) ) {
-					jQuery( "#form-dialog" ).click().attr( "method", "GET" ).ajaxSubmit( function( response ) {
-						var name = response.data.name;
-						jQuery( "#form-dialog #name_contact_call" ).attr( "value" , name );
-						var society = response.data.society;
-						jQuery( "#form-dialog #society_contact_call" ).attr( "value" , society );
-						var email = response.data.mail;
-						jQuery( "#form-dialog #email_contact_call" ).attr( "value" , email );
-						var number = response.data.number;
-						jQuery( "#form-dialog #number_contact_call" ).attr( "value" , number );
-						var comment = response.data.commentcontent;
-						if ( null === comment ) { var comment = "Inconnu / Not found" }
-						jQuery( "#form-dialog #comment_content_call" ).attr( "value" , comment );
-					});
-				}	else {
-					var comment = "E-mail non valide !";
-					jQuery( "#form-dialog #comment_content_call" ).attr( "value" , comment );
-				}
-			},
+			// Rechercher: function() {
+			// 	var emailCheck = jQuery( "#form-dialog #email_contact_call" ).val();
+			// 	if ( validateEmail( emailCheck ) ) {
+			// 		jQuery( "#form-dialog" ).click().attr( "method", "GET" ).ajaxSubmit( function( response ) {
+			// 			var name = response.data.name;
+			// 			jQuery( "#form-dialog #name_contact_call" ).attr( "value" , name );
+			// 			var society = response.data.society;
+			// 			jQuery( "#form-dialog #society_contact_call" ).attr( "value" , society );
+			// 			var email = response.data.mail;
+			// 			jQuery( "#form-dialog #email_contact_call" ).attr( "value" , email );
+			// 			var number = response.data.number;
+			// 			jQuery( "#form-dialog #number_contact_call" ).attr( "value" , number );
+			// 			var comment = response.data.commentcontent;
+			// 			if ( null === comment ) { var comment = "Inconnu / Not found" }
+			// 			jQuery( "#form-dialog #comment_content_call" ).attr( "value" , comment );
+			// 		});
+			// 	}	else {
+			// 		var comment = "E-mail non valide !";
+			// 		jQuery( "#form-dialog #comment_content_call" ).attr( "value" , comment );
+			// 	}
+			// },
 			OK: function() {
 				var emailCheck = jQuery( "#form-dialog #email_contact_call" ).val();
 				if ( validateEmail( emailCheck ) ) {
